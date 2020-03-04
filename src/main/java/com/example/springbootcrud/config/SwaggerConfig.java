1265b964d45d9b8905456c9509e11a2ff82a00b9
ialkouz <ialkouz@fact.com.jo>
2020-03-04 11:53:59 +0200

    add swagger api docs

---
+++ src/main/java/com/example/springbootcrud/config/SwaggerConfig.java
@@ -0,0 +1,30 @@
+package com.example.springbootcrud.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ParameterBuilder;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static java.util.Collections.singletonList;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket apiDocket() {
+        Docket docket = new Docket(DocumentationType.SWAGGER_2)
+                .globalOperationParameters(singletonList(
+                        new ParameterBuilder()
+                                .name("Authorization")
+                                .modelRef(new ModelRef("string"))
+                                .parameterType("header")
+                                .required(false)
+                                .hidden(true)
+                                .build()));
+        return docket;
+
+    }
+}
--- src/main/java/com/example/springbootcrud/config/WebSecurityConfig.java
+++ src/main/java/com/example/springbootcrud/config/WebSecurityConfig.java
@@ -50,7 +50,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 // We don't need CSRF for this example
         httpSecurity.csrf().disable()
 // dont authenticate this particular request
-                .authorizeRequests().antMatchers("/authenticate").permitAll().
+                .authorizeRequests().antMatchers("/authenticate","/swagger-ui.html/**","/v2/**","/swagger*/**","/webjars/**").permitAll().
 // all other requests need to be authenticated
         anyRequest().authenticated().and().
 // make sure we use stateless session; session won't be used to
